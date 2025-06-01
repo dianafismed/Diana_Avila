3b00e7a95e32983acf1a3ab8bd16711462207467
Diana Avila <missernightangel@gmail.com>
2025-06-01 17:56:24 -0300

    projects

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
-import { GradesComponent } from '../pages/grades/grades.component';
-import { PrincipalComponent } from '../pages/principal/principal.component';
+import { GradesComponent } from './pages/grades/grades.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 export const routes: Routes = [
-  { path: '', component:PrincipalComponent},
-  { path: 'grades', component: GradesComponent }
+  { path: '', component: PrincipalComponent },
+  { path: 'grades', component: GradesComponent },
+  { path: 'projects', component: ProjectsComponent },
+
 ];
